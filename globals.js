b152d833e1f35a12834fb8bd326741c24dad39db
elopezf <elopez@mahisoft.com>
2016-10-25 16:28:26 -0400

    New version of List a property

---
+++ globals.js
@@ -0,0 +1,19 @@
+var variables = {
+    user: "elopez@mahisoft.com",
+    password: "123456",
+    dir: 'Address',
+    zipcodes: ['92602', '92618', '92620', '92672', '92688', '92037', '93637', '94107', '94536'],
+    path: './tests_output/screenshots/',
+    ext: '.png',
+    sqft: '2000',
+    lsSqft: '5000',
+    yearBuilt: '1995',
+    amount: '350000',
+    image: '\\home.jpg',
+    userce: "jose",
+    passwordce: "jose"
+}
+
+module.exports = {
+    variables: variables
+}
